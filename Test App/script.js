e0010ad4c46df0a93480155a65d8d1a742ffe191
Simar <simar022netsmartz@gmail.com>
2026-02-02 20:24:09 +0530

    Commit03

--- Test App/script.js
+++ Test App/script.js
@@ -4,8 +4,8 @@ const carData = [
     { id: 3, name: "Phantom V16", cat: "sport", price: "$1.8M", img: "https://images.unsplash.com/photo-1503376780353-7e6692767b70?auto=format&fit=crop&w=800" },
     { id: 4, name: "Zenith RS", cat: "motorsport", price: "$3.4M", img: "https://images.unsplash.com/photo-1583121274602-3e2820c69888?auto=format&fit=crop&w=800" },
     { id: 5, name: "Monolith GT", cat: "sport", price: "$980K", img: "https://images.unsplash.com/photo-1617788131775-ceb2027fd12c?auto=format&fit=crop&w=800" },
-    { id: 6, name: "Volt Apex S", cat: "electric", price: "$1.2M", img: "https://images.unsplash.com/photo-1580273916550-e323be2ae537?auto=format&fit=crop&w=800" },
-    { id: 7, name: "Black Label GTR", cat: "motorsport", price: "$5.8M", img: "https://images.unsplash.com/photo-1603584173870-7f300f2e030c?auto=format&fit=crop&w=800" },
+    // { id: 6, name: "Volt Apex S", cat: "electric", price: "$1.2M", img: "https://images.unsplash.com/photo-1580273916550-e323be2ae537?auto=format&fit=crop&w=800" },
+    // { id: 7, name: "Black Label GTR", cat: "motorsport", price: "$5.8M", img: "https://images.unsplash.com/photo-1603584173870-7f300f2e030c?auto=format&fit=crop&w=800" },
     { id: 8, name: "Legacy V12", cat: "sport", price: "$2.9M", img: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?auto=format&fit=crop&w=800" }
 ];
 
